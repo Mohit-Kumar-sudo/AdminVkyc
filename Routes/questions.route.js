efa7c37a29a56046a692c87321e67c400bf826e3
Mohit-Kumar-sudo <mohitvishkarma50@gmail.com>
2023-05-27 13:57:36 +0530

    backend push

---
+++ Routes/questions.route.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const Controller = require('../Controllers/question.controller')
+
+router.post('/', Controller.create)
+
+router.get('/:id', Controller.get)
+
+router.get('/', Controller.list)
+
+router.put('/:id', Controller.update)
+
+router.delete('/:id', Controller.delete)
+
+router.put('/:id/restore', Controller.restore)
+
+router.get('/videoKyc/:instrumentId/:partyTypeId', Controller.getQuestions)
+
+router.get('/filter/:instrumentId/:partyTypeId', Controller.getQuestions)
+
+module.exports = router
