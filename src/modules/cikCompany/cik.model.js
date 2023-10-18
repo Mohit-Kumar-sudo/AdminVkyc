cf915d485d6edadd51da8d567ea74889fe1e6100
Mohit Kumar <mohitvishkarma50@gmail.com>
2023-10-18 15:37:07 +0530

    Face Name Matrix

--- src/modules/cikCompany/cik.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-
-const cikCompany = new Schema({
-    title: { type: Schema.Types.String },
-    cik_str: { type: Schema.Types.Number },
-    ticker: { type: Schema.Types.String }
-});
-
-module.exports = mongoose.model('cikCompany', cikCompany);
