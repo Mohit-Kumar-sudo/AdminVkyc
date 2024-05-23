01755c158d9bc17bcf2d275b3b2f848d36d75829
Mohit Kumar <mohitvishkarma50@gmail.com>
2024-05-23 18:55:13 +0530

    platform

--- modules/google_api/google_api.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import * as googleAPIController from './google_api.controllers';
-import {authJwt} from '../../services/auth.services';
-
-const routes = new Router();
-
-routes.get('/:google_search_query', authJwt, googleAPIController.getGoogleNews);
-
-export default routes;
