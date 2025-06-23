718349377d78d8d05a3e90cc1421b65fa7dcdeb0
Mohit Kumar <mohitvishkarma50@gmail.com>
2025-06-23 18:38:00 +0530

    demo

---
+++ src/modules/resources/chart/chart.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import validate from 'express-validation';
+
+import { authJwt } from '../../../services/auth.services';
+
+import * as chartController from './chart.controller';
+import chartValidation from './chart.validations';
+
+const routes = new Router();
+routes.get("/getChartsTitle", chartController.getChartCount)
+routes.get('/:rid*?/', authJwt, validate(chartValidation.getChartData), chartController.getChartData);
+
+export default routes;
