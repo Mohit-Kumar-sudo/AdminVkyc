a52a92fe20633961e0569a7cf5f1a1b1ba977c55
Mohit Kumar <mohitvishkarma50@gmail.com>
2025-11-06 16:37:36 +0530

    new

--- backend/src/modules/sec-data/sec_data.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as secDataController from './sec_data.controllers';
-
-const routes = new Router();
-
-routes.get('/sec-data/:searchQuery', secDataController.getSecData);
-
-export default routes;
--- backend/src/modules/videos-filter/videos-filter.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import * as videoController from './videos-filter.controller';
-
-const routes = new Router();
-
-routes.get('/videos-filter', videoController.getVideos);
-
-export default routes;
