330f12cca618c1fb80f6b00c5d43cd6ad5f7c520
Mohit Kumar <mohitvishkarma50@gmail.com>
2025-11-24 17:23:03 +0530

    new

--- Backend/Helpers/generatePassword.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto");
-
-function generatePassword(length = 10) {
-  return crypto.randomBytes(length).toString("base64").slice(0, length);
-}
-
-module.exports = generatePassword;
